bb4c5fb25c325d3412590f44e0ae6f489343b22b
callofduty890 <1032031912@qq.com>
2019-08-10 21:00:26 +0800

    主登录窗口的显示-- 1.显示登录窗口 2.判断登录窗口是OK还是退出状态

--- StudentManager/Program.cs
+++ StudentManager/Program.cs
@@ -17,8 +17,23 @@ namespace StudentManager
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+            //创建登录窗口
+            FrmUserLogin objFrmLogin = new FrmUserLogin();
+            //设定为当前窗口所有者控制-也就是这个窗体结束后才能接着下面运行
+            DialogResult result = objFrmLogin.ShowDialog();
 
-            Application.Run(new FrmMain());
+            //判断登录是否成功
+            if (result == DialogResult.OK)
+            {
+                //运行主窗体
+                Application.Run(new FrmMain());
+            }
+            else
+            {
+                //退出整个应用程序
+                Application.Exit();
+            }
+            
         }
 
     }
