e9c051c555a331f2f2414e5a9909f1f37890c364
callofduty890 <1032031912@qq.com>
2019-08-12 15:47:33 +0800

    >>>根据学号查询学员信息<<< 1.完成显示窗口的显示

--- StudentManager/FrmStudentInfo.cs
+++ StudentManager/FrmStudentInfo.cs
@@ -19,10 +19,16 @@ namespace StudentManager
         //注意this()会使用FrmStudentIfo()这个构造函数
         public FrmStudentInfo(StudentExt objStudent):this()
         {
-
             //显示学员信息
             this.lblStudentName.Text = objStudent.StudentName;
             this.lblStudentIdNo.Text = objStudent.StudentIdNo;
+            this.lblAddress.Text = objStudent.StudentAddress;
+            this.lblBirthday.Text = objStudent.Birthday.ToShortDateString();
+            this.lblCardNo.Text = objStudent.CardNo;
+            this.lblClass.Text = objStudent.ClassName;
+            this.lblGender.Text = objStudent.Gender;
+            this.lblPhoneNumber.Text = objStudent.PhoneNumber;
+
         }
 
         //关闭
